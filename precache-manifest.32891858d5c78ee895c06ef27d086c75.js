9e2ea3494ca17f1dd0b7bec4772a594961d6a316
zwillerzoog <marsh.heidi.c@gmail.com>
2019-10-20 12:50:17 -0400

    Updates

---
+++ precache-manifest.32891858d5c78ee895c06ef27d086c75.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8f9d92fface4353cae17073155f24b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9579bdab9f835ee1f8de",
+    "url": "/react-portfolio/static/css/main.b1b561a0.chunk.css"
+  },
+  {
+    "revision": "bc29dd6d397372f56daf",
+    "url": "/react-portfolio/static/js/2.00eac186.chunk.js"
+  },
+  {
+    "revision": "9579bdab9f835ee1f8de",
+    "url": "/react-portfolio/static/js/main.8ed5a73b.chunk.js"
+  },
+  {
+    "revision": "7645cd71f3434ecda4f7",
+    "url": "/react-portfolio/static/js/runtime-main.12f49f2d.js"
+  },
+  {
+    "revision": "0e3b31083289da636bf66895f7db2109",
+    "url": "/react-portfolio/static/media/feedback_screenshot.0e3b3108.png"
+  },
+  {
+    "revision": "890df557fef312eff651ee3debbe48fa",
+    "url": "/react-portfolio/static/media/headshot_pic.890df557.jpeg"
+  },
+  {
+    "revision": "55fd43550091f19124433d46b5d31a87",
+    "url": "/react-portfolio/static/media/rex_screenshot.55fd4355.png"
+  },
+  {
+    "revision": "9ca2d6ce2ddea21ca143a076b2203907",
+    "url": "/react-portfolio/static/media/running-horse-once.9ca2d6ce.gif"
+  },
+  {
+    "revision": "7c5533ae9b1ce0dfa4b94bfd964d1b4d",
+    "url": "/react-portfolio/static/media/savage_screenshot.7c5533ae.png"
+  }
+]);
